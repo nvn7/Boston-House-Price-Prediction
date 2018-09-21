70dbda5a8487962fe687b5d5db85b12435367afe
nvn7 <naveenkumarnvn7@gmail.com>
2018-09-22 03:08:30 +0530

    after adding styles

--- model.py
+++ model.py
@@ -60,4 +60,4 @@ client_data = [v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13]
 
 price = reg.predict([client_data])
 
-print("Predicted Selling Price of a house is : $ %.2f"%price)
+print("Predicted Selling Price of your house is : $ %.2f"%price)
